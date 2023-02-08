65946f56cdf28e50bfde0f50f943d5419f859281
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-02-07 22:37:55 -0300

    Criando dois usuarios na seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name:"Caetano Rei Delas", enrollment:211068666, is_admin:true, is_student:true, is_teacher:true, email:"caetanosocafofo@infiel.com", password:"soudelaseparaelas")
+User.create(name:"De sá trepa no C.A", enrollment:211068999, is_admin:true, is_student:true, is_teacher:true, email:"desáinbroxavel@cachorro.com", password:"foguetenãodáré")
