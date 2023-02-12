a910ae647ef105af313945d14fb4e87dbb9c3218
Luiz Henrique <luizhfs09@gmail.com>
2023-02-12 12:54:55 -0300

    :wrench: consertando o factorybot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,11 +2,16 @@ FactoryBot.define do
   sequence :user_email do |n|
     "test#{n}@test"
   end
+
+  sequence :user_enrollment do |n|
+    123_456_789 + n
+  end
+
   factory :user do
     email {generate(:user_email)}
     password { '0123456' }
     name { 'Teste' }
-    enrollment { 123_456_789 }
+    enrollment { generate(:user_enrollment) }
     is_admin { true }
     is_teacher { true }
     is_student { true }
